1823f339b8327087c1127184106293d752625ced
Nikki Cheah <nikkiczx@gmail.com>
2021-09-16 21:40:02 +0800

    Remove Udacity project from github pages

--- Udacity-FS-JS-Nanodegree/01-Backend-Dev-Project/src/utilities/fileExists.ts
+++
@@ -1,9 +0,0 @@
-import { existsSync as fsExists } from "fs";
-
-export function fileExists(fileLoc: string): boolean {
-  if (fsExists(fileLoc)) {
-    return true;
-  } else {
-    return false;
-  }
-}
